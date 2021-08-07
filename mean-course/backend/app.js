2d92ea20db491e128b6a2ad40b556dabe560ab84
apip <apip@gitlab.lab>
2021-08-07 12:42:06 +0700

    reorganize backend

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// konfigurasi mongo db
-require('./utils/db');
-const PostModel = require('./models/post');
+const postRoutes = require('./routes/posts');
 
 // konfigurasi express
 const app = express();
 
+// konfigurasi mongo db
+require('./utils/db');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// konfigurasi CORS Origin
+// konfigurasi agar tidak kena CORS Origin
 app.use((req, res, next) => {
     res.setHeader(
         "Access-Control-Allow-Origin",
@@ -28,59 +29,7 @@ app.use((req, res, next) => {
     next();
 });
 
-
-// method APIS
-app.post('/api/posts', (req, res, next) => {
-    const post = new PostModel({
-        title: req.body.title,
-        content: req.body.content
-    });
-    post.save().then(createdPost => {
-        res.status(201).json({
-            message: 'Post added successfuly!',
-            postId: createdPost._id
-        });
-    });
-});
-app.put('/api/posts/:id', (req, res, next) => {
-    const post = new PostModel({
-        _id: req.body.id,
-        title: req.body.title,
-        content: req.body.content
-    });
-    PostModel.updateOne({ _id: req.params.id }, post)
-        .then(result => {
-            console.log(result);
-            res.status(200).json({ message: 'Update successfuly' });
-        });
-});
-app.get('/api/posts', (req, res, next) => {
-    PostModel.find()
-        .then((documents) => {
-            res.status(200).json({
-                message: 'post fetched succesfully',
-                posts: documents
-            });
-        });
-});
-app.get('/api/posts/:id', (req, res, next) => {
-    PostModel.findById(req.params.id)
-        .then((post) => {
-            if (post) {
-                res.status(200).json(post);
-            } else {
-                res.status(404).json({
-                    message: 'post not found'
-                });
-            }
-        });
-});
-app.delete('/api/posts/:id', (req, res, next) => {
-    PostModel.deleteOne({ _id: req.params.id }).then(result => {
-        res.status(200).json({
-            message: 'Post deleted!'
-        });
-    });
-});
+// konfigurasi method APIS
+app.use('/api/posts', postRoutes);
 
 module.exports = app;
