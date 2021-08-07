7ab2e1b7cf9c3061c06be1215465c17c7670f95b
apip <apip@gitlab.lab>
2021-08-07 13:25:03 +0700

    edit layoutmodule

--- mean-course/src/app/shared/layout/layout.module.ts
+++ mean-course/src/app/shared/layout/layout.module.ts
@@ -1,11 +1,6 @@
 import { NgModule } from '@angular/core';
-
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
 @NgModule({
-    declarations: [
-        PageNotFoundComponent
-    ],
+    declarations: [],
     imports: [],
     exports: []
 })
