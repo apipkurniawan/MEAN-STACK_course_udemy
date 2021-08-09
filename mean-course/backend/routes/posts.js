82dc12ac29c4ac191690243acad08101c7f3626c
apip <apip@gitlab.lab>
2021-08-09 23:21:31 +0700

    adding middleware to protect user

--- mean-course/backend/routes/posts.js
+++ mean-course/backend/routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const PostModel = require('../models/post');
 const multer = require('multer');
 
+const checkAuth = require('../middleware/check-auth');
 const router = express.Router();
 
 // konfigurasi penyimpanan image
@@ -28,7 +29,7 @@ const storage = multer.diskStorage({
 
 // method APIS
 // simpan
-router.post('', multer({ storage: storage }).single('image'), (req, res, next) => {
+router.post('', checkAuth, multer({ storage: storage }).single('image'), (req, res, next) => {
     const url = `${req.protocol}://${req.get('host')}`;
     const post = new PostModel({
         title: req.body.title,
@@ -47,7 +48,7 @@ router.post('', multer({ storage: storage }).single('image'), (req, res, next) =
 });
 
 // ubah
-router.put('/:id', multer({ storage: storage }).single('image'), (req, res, next) => {
+router.put('/:id', checkAuth, multer({ storage: storage }).single('image'), (req, res, next) => {
     let imagePath = req.body.imagePath;
     if (req.file) {
         const url = `${req.protocol}://${req.get('host')}`;
@@ -104,7 +105,7 @@ router.get('/:id', (req, res, next) => {
 });
 
 // deleteById
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', checkAuth, (req, res, next) => {
     PostModel.deleteOne({ _id: req.params.id }).then(result => {
         res.status(200).json({
             message: 'Post deleted!'
