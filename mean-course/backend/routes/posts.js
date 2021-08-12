bb85c1d876bcd84e1a8699ad4829b823eeff6a2d
apip <apip@gitlab.lab>
2021-08-12 21:17:50 +0700

    fixing update bug

--- mean-course/backend/routes/posts.js
+++ mean-course/backend/routes/posts.js
@@ -1,36 +1,15 @@
 const express = require('express');
-const multer = require('multer');
 
 const PostController = require('../controllers/post');
 const checkAuth = require('../middleware/check-auth');
+const extractFile = require('../middleware/file');
+
 const router = express.Router();
 
-// konfigurasi penyimpanan image
-const MIME_TYPE_MAP = {
-    'image/png': 'png',
-    'image/jpg': 'jpg',
-    'image/jpeg': 'jpg'
-};
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const isValid = MIME_TYPE_MAP[file.mimetype];
-        let error = new Error('inValid mime type');
-        if (isValid) {
-            error = null;
-        }
-        cb(error, 'backend/images');
-    },
-    filename: (req, file, cb) => {
-        const name = file.originalname.toLowerCase().split(' ').join('-');
-        const ext = MIME_TYPE_MAP[file.mimetype];
-        cb(null, `${name}-${Date.now()}.${ext}`);
-    }
-});
-
 // method APIs
-router.post('', checkAuth, multer({ storage: storage }).single('image'), PostController.savePost);
+router.post('', checkAuth, extractFile, PostController.savePost);
 
-router.put('/:id', checkAuth, multer({ storage: storage }).single('image'), PostController.updatePost);
+router.put('/:id', checkAuth, extractFile, PostController.updatePost);
 
 router.get('', PostController.getAllPosts);
 
